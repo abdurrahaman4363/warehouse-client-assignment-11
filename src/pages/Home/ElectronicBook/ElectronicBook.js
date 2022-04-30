34960ea1090088c5d8f23dd449543a8fb5547a94
abdurrahaman4363 <abdur.rahaman4363@gmail.com>
2022-04-30 10:37:06 +0600

    added a section which is electronic

--- src/pages/Home/ElectronicBook/ElectronicBook.js
+++ src/pages/Home/ElectronicBook/ElectronicBook.js
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const ElectronicBook = () => {
+    const [book, setBook] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/electronic')
+            .then(res => res.json())
+            .then(data => setBook(data))
+    }, [])
     return (
         <div>
             <h2 className='text-center text-info'>Electronic Books</h2>
+            <div className='programming'>
+                {
+                    book.map(b => <div className='programming-card' key={b._id}>
+                        <img src={b.picture} alt="" />
+
+                        <div>
+                            <h2>Name:{b.name}</h2>
+                            <p>Price:{b.price}</p>
+                            <p>{b.description}</p>
+                        </div>
+
+                    </div>)
+                }
+            </div>
         </div>
     );
 };
